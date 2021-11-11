9461f82bb65846f15d1acfc991245f0bcd83a527
Tomasz <tomaszbochnak00@gmail.com>
2021-11-11 21:32:32 +0100

    event listeners keys, ctrlkey + click to manipulate Node

---
+++ lab-3/ex-8/scripts/script.js
@@ -0,0 +1,54 @@
+class BloonProperies {
+    constructor() {
+        this.popped = false;
+        this.currentSize = 100;
+    }
+}
+
+
+function updateSize(element, size) {
+    element.style.fontSize = size + "px";
+}
+
+
+function actualSizeInfo(element, bloonProperties, sizeInfoElement) {
+    // removing inner after bloon poop
+    if (bloonProperties.popped === true) {
+        sizeInfoElement.innerHTML = '';
+    } else if (element.ctrlKey) {
+        // control key was down
+        sizeInfoElement.innerHTML = `size: ${bloonProperties.currentSize}`;
+    }
+}
+
+
+function changeSize(e, bloonProperties, bloon, sizeInfoElement) {
+    if (e.key === 'ArrowUp' && !bloonProperties.popped) {
+        bloonProperties.currentSize = Math.min(500, bloonProperties.currentSize + 10);
+        updateSize(bloon, bloonProperties.currentSize);
+
+    } else if (e.key === 'ArrowDown' && !bloonProperties.popped) {
+        bloonProperties.currentSize = Math.max(20, bloonProperties.currentSize - 10);
+        if (bloonProperties.currentSize == 20) {
+            bloonProperties.popped = true;
+            bloon.classList.remove('bloon')
+            bloon.classList.add('popped')
+            updateSize(bloon, 200);
+            actualSizeInfo(null, bloonProperties, sizeInfoElement);
+            return;
+        }
+        updateSize(bloon, bloonProperties.currentSize);
+    }
+}
+
+
+function bloonManipulation() {
+    const bloonElement = document.querySelector('.emoji');
+    const bloonClassElement = document.querySelector('.bloon');
+    const sizeInfoElement = document.querySelector('.size-info');
+    bloonProperties = new BloonProperies();
+    addEventListener('keydown', (e) => changeSize(e, bloonProperties, bloonElement, sizeInfoElement));
+    bloonClassElement.addEventListener('click', (e) => actualSizeInfo(e, bloonProperties, sizeInfoElement));
+}
+
+window.onload = bloonManipulation
